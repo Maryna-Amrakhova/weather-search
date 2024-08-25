5607818375fc9b7017a2db931d30f9678cfe6ee1
Maryna-Amrakhova <seagull2305@gmail.com>
2024-08-25 17:03:34 +0300

    added a functionality API

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+function apdateWeather(response) {
+    let currentTemperature = document.querySelector("#temp");
+    currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "btf3a0c41c82o04bde5657e178c809b4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(apdateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
   let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#main-form");
